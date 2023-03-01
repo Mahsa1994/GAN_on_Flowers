7e70e456c716da0ee863ad8ef63f86e468339644
Mahsa1994 <30629449+Mahsa1994@users.noreply.github.com>
2023-03-01 20:57:38 +0330

    Update prepare_dataset.py

--- dataset/prepare_dataset.py
+++ dataset/prepare_dataset.py
@@ -1,9 +1,4 @@
-import os
 import tensorflow as tf
-
-
-
-"""Load and prepare the given dataset"""
 
 
 def load_dataset(dataset_path, img_size, batch_size, val_split_size):
@@ -31,23 +26,12 @@ def load_dataset(dataset_path, img_size, batch_size, val_split_size):
 
     num_classes = 5  # dataset_info.features['label'].num_classes
 
-    num_training_examples = 0
-    num_validation_examples = 0
-
-    for example in train_set:
-        num_training_examples += 1
-
-    for example in val_set:
-        num_validation_examples += 1
-
     for i, example in enumerate(train_set.take(10)):
         print('Image {} shape: {} label: {}'.format(i + 1, example[0].shape, example[1]))
 
     print('Total Number of Classes: {}'.format(num_classes))
-    print('Total Number of Training Images: {}'.format(num_training_examples))
-    print('Total Number of Validation Images: {} \n'.format(num_validation_examples))
+    print('Total Number of Training Images: {}'.format(len(train_set)))
+    print('Total Number of Validation Images: {} \n'.format(len(val_set)))
 
-    return train_set, val_set, num_training_examples, num_validation_examples
+    return train_set, val_set, len(train_set), len(val_set)
 
-
-# load_dataset()
